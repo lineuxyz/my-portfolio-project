8304d8d965ec8def71c69f436003935393ebd2fc
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-14 17:11:14 -0300

    component style added

---
+++ src/components/Three/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    padding: 0 30px;
+    margin: 80px 0;
+    justify-content: space-between;
+    width: 100%;
+    
+
+    div {
+        width: 50%;
+    }
+`;
+
+export const Suport = styled.div `
+    
+`
+
+export const Title = styled.div `
+    color: #fff;
+    text-align: center;
+    font-size: 23px;
+    font-weight: bold;
+    margin-bottom: 30px;
+`
+
+export const FirstItem = styled.div `
+   color: #C1C1C1;
+   text-align: left;
+   font-size: 17px;
+   font-weight: bold;
+   display: flex;
+   margin: 0 0 30px 0;
+
+   img {
+       margin-left: 30px;
+   }
+`
+
+export const SecundItem = styled.div `
+   color: #C1C1C1;
+   text-align: left;
+   font-size: 17px;
+   font-weight: bold;
+   display: flex;
+   margin: 0 0 30px 0;
+
+   img {
+       margin-left: 30px;
+   }
+`
+
+export const ThreeItem = styled.div `
+   color: #C1C1C1;
+   text-align: left;
+   font-size: 17px;
+   font-weight: bold;
+   display: flex;
+   margin: 0 0 30px 0;
+
+   img {
+       margin-left: 30px;
+   }
+`
