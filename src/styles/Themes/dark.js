842fab90775fec33e3d0f36d5aa013cc041594e4
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-15 16:55:31 -0300

    added dark and light theme settings

---
+++ src/styles/Themes/dark.js
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+  
+    colors: {
+      primary: '#333',
+      secundary: '#FFC400',
+  
+      background: '#313131',
+      text: '#fff'
+    },
+  }
---
+++ src/styles/Themes/light.js
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+  
+    colors: {
+      primary: '#e9e9e9',
+      secundary: '#FFC400',
+  
+      background: '#E9E9E9',
+      text: '#393939'
+    },
+  }
