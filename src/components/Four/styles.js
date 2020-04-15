0f5e5393c4492fd57d9c299003517b539e3f9db7
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-15 16:54:41 -0300

    Component styling

---
+++ src/components/Four/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    z-index: 0;
+
+    div {
+        width: 50%;
+    }
+`;
+
+export const Title = styled.div`
+   color: #C1C1C1;
+   text-align: left;
+   font-size: 17px;
+   font-weight: bold;
+   display: flex;
+`
+
+export const FirstItem = styled.div``
+
+export const SecundItem = styled.div``
+
+export const ThreeItem = styled.div``
+
