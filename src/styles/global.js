b2d12781ad926acfb136cedef55b11600289529d
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-22 13:35:14 -0300

    improvement for responsive mode and also adding themes

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import px2vw from '../utils/px2vw';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap');
@@ -8,16 +9,31 @@ export default createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
+  html {
+    font-size: 62.5%;
+  }
   html, body, #root {
     height: 100%;
   }
   body {
-    font: 14px 'Roboto', sans-serif;
-    background: #313131;
+    font: 1.6rem 'Roboto', sans-serif;
     overflow-x: hidden;
     -webkit-font-smoothing: antialiased !important;
+    background-color: #313131;
   }
   ul {
     list-style: none;
   }
+
+  @media (max-width: 850px) {
+    html {
+      font-size: 50%;
+    }
+  }
+
+  /*@media (max-width: 850px) {
+    html {
+      font-size: 50%;
+    }
+  } */
 `
