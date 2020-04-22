45bfea83c3bf90c025ac004351f49d71df6415be
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-22 13:24:44 -0300

    made changes for better accessibility and responsive mode

--- src/components/First/index.js
+++ src/components/First/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-scroll'
 
 import persoImage from '../../assets/coding.png'
 import { Container, Title, MainText, FirstButton } from './styles';
 
 export default function Board() {
   return (
-    <Container>
+    <Container id="first">
         <div>
-            <Title>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</Title>
-            <MainText>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam</MainText>
-            <FirstButton>Mais sobre mim</FirstButton>
+            <Title>Lorem ipsum dolor sit amet</Title>
+            <MainText>Lorem ipsum dolor sit amet, consetetur sadipscing elitr<br/>, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam</MainText>
+            <FirstButton><Link to="secund" smooth={true} duration={2000}>Mais sobre mim</Link></FirstButton>
         </div>
         <img src={persoImage} width={635} alt="Imagem de um personagem codando" />
     </Container>
