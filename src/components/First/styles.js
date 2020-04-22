3ada0985a045cc2135ee89c50ad8e6dfa57200b0
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-22 13:26:01 -0300

    styling for the application to be responsive

--- src/components/First/styles.js
+++ src/components/First/styles.js
@@ -4,36 +4,75 @@ export const Container = styled.div`
     display: flex;
     padding: 0 30px;
     justify-content: space-between;
-    width: 100%;
+    width: 90%;
     z-index: 0;
 
     div {
-        width: 50%;
+        width: 90%;
+        max-width: 998px !important;
     } 
 
     img {
+        width: 635px;
         position: absolute;
         right: 0;
         top: -12px;
-        padding-right: 0
-       
+        padding-right: 0;
     }
+    @media (max-width: 1000px) {
+    img {
+      width: 520px; 
+    }
+  }
+    @media (max-width: 850px) {  
+      flex-direction: column;
+        width: 100%;
+        align-items: center;
+
+      img {
+          position: relative;
+          width: 380px;
+          order: -1;
+          margin-top: 30px;
+      }
+
+      p {
+          margin: 0;
+          text-align: center;
+          width: 100%;
+      }
+
+      button {
+          margin: 0 auto;
+          margin-top: 60px;
+      }
+  }
+  @media (max-width: 850px) {
+    img {
+          width: 280px;
+    }
+  }    
+
 `;
 
 export const Title = styled.p`
-    font-size: 30px;
+    font-size: 3.0rem;
     color: #FFC400;
     margin: 60px 0;
+    width: 600px;
+
 `
 
 export const MainText = styled.p `
     width: 50%;
-    font-size: 15px;
+    font-size: 1.6rem;
     color: #C1C1C1;
     margin-left: 60px;
 `
 
-export const FirstButton = styled.button`
+export const FirstButton = styled.button.attrs({
+    type: 'button'
+})`
     display: flex;
     width: 182px;
     height: 33px;
@@ -44,5 +83,12 @@ export const FirstButton = styled.button`
     color: #fff;
     border-radius: 20px;
     justify-content: center;
-    align-items: center
+    align-items: center;
+
+    &:hover {
+        -moz-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+        transition: 0.2s;
+        }
 `
