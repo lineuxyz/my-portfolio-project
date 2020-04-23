8a695ef3a2e12bf346f26e4541aac15b6de56d3f
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-23 17:52:42 -0300

    stylization finished

--- src/components/_Header/styles.js
+++ src/components/_Header/styles.js
@@ -1,17 +1,39 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div`
+export const Container = styled.header`
   height: 40px;
-  background: #313131;
   padding: 0 30px;
   color: #fff;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  font-weight: bold;
   flex-wrap: wrap;
-  
+
+  margin-right: 15px;
+
+  div {
+    display: flex;
+
+    p {
+      margin-left: 15px;
+    }
+
+  }
+
+  @media (max-width: 530px) {
+      display: none;
+      flex-flow: column nowrap;
+      background-color: #FFC400;
+      position: fixed;
+      top: 0;
+      right: 0;
+      height: 100vh;
+      width: 300px;
+      padding-top: 3.5rem;
+  }
 `;
+
 
 export const List = styled.ul`
   display: inline-flex;
@@ -20,15 +42,46 @@ export const List = styled.ul`
   z-index: 1;
 
   a {
-    margin: 0 20px;
+    margin: 0 20px;      
+    cursor: pointer;
+
+    &::before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: #6699ff;
+        visibility: hidden;
+        -webkit-transform: scaleX(0);
+        transform: scaleX(0);
+        -webkit-transition: all 0.3s ease-in-out 0s;
+        transition: all 0.3s ease-in-out 0s;
+    }
+
+    &:hover {
+        color: #6699ff;
+
+        &::before {
+            visibility: visible;
+            -webkit-transform: scaleX(1);
+            transform: scaleX(1);
+        }
+    }
+  }
+
+  svg {
+    margin: 0 20px;      
+    color: #313131;
+    /*cursor: pointer;
 
     &:hover {
       -moz-transform: scale(1.1);
       -webkit-transform: scale(1.1);
       transform: scale(1.1);
       transition: 0.2s;
-      color: #F6511D;
-      cursor: pointer;
-    }
+      color: ${shade(0.2, '#313131')}
+    }*/
   }
-`
+`;
