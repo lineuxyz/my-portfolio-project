24ac6acbab26a395d4d9c49630ebc939ee6b2a34
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-25 09:24:21 -0300

    finished companies index for deploy

---
+++ src/components/Companies/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Footer, List, Title, Copy } from './styles';
+
+import city from '../../assets/city.png'
+
+export default function Four() {
+  return (
+    <>
+    <Footer id="four">  
+            <div>
+                <img src={city} width={500} alt="qualquer coisa" />
+            </div>
+            <div>
+                    
+                    <List>
+                    <p>My career</p>
+                        <ul><Title><span>Company Name:</span> Itaú at Oliver agency | 2019/Dec to current day</Title>
+                        <li key="company-informations">Front-end developer, working on the bank's performance side, working with multidisciplinary teams, including UX designer, Art Director, Motion, UX writer, using agile methodologies for better delivery.</li>
+                        </ul>
+                    </List>
+        
+                    <List>
+                        <ul><Title><span>Company Name:</span> 99 Freelas | 2019/May to current day</Title>
+                        <li>When I have time left, I work as a freelance programmer, currently using the 99 freelas platform, there I develop several types of projects, from small statistical pages, changes and code improvements.</li>
+                        </ul>
+                    </List>
+
+                    <List>
+                        <ul><Title><span>Company Name:</span> Google | 2018 to 2019/May</Title>
+                        <li>Care of some open source repositories from Google, I did maintenance, corrections, conflict resolution of pull requests, this was a Google project, where there was a small contract also acting as a freelance.</li>
+                        </ul>
+                    </List>
+            </div>
+    </Footer>
+      <Copy>
+      <p>Lineu © All rights reserved</p>
+      <a  target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jo%C3%A3o-victor-sousa-c-gomes/"> Designer by Joao Victor Sousa</a>
+      </Copy>
+     </> 
+  );
+}
