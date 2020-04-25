640c6ac976b08a721a2234fa1ee53b0bddd4d3e0
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-25 12:04:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,4 @@ import App from './App';
 
 Sentry.init({dsn: "https://e2d474651d56461cab7c5e2130aec577@o303918.ingest.sentry.io/5212475"});
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
