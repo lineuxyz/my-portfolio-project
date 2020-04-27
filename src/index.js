637c77259fb3023814dbc7b9f7105038ad11a7c4
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-27 15:59:11 -0300

    add sentry integration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import App from './App';
 
-Sentry.init({dsn: "https://e2d474651d56461cab7c5e2130aec577@o303918.ingest.sentry.io/5212475"});
+Sentry.init({dsn: "https://e1dc5521e0ad4a08a0ad0cbd17117d59@o303918.ingest.sentry.io/5215263"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
