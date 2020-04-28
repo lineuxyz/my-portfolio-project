baf30c22a2b5bc74414eac4081e332a4d7719290
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-04-28 09:52:41 -0300

    initial form implementation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <Description />
     <Learning />
     <Companies />
+
     <GlobalStyle />
   </>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
 import App from './App';
-
-Sentry.init({dsn: "https://e2d474651d56461cab7c5e2130aec577@o303918.ingest.sentry.io/5212475"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
