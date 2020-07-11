57dd9df7e466d0d057e293af2374fd33922cd79f
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-11 14:00:06 -0300

    created the footer on new component, index and style

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Copy = styled.div`
+      background-color: #353535;
+      display: flex;
+      justify-content: space-between;
+      padding: 15px 40px 10px 40px;
+      color: #fff;
+      font-size: 1.1rem;
+    a {
+      color: #FFC400;
+  }
+
+`;
