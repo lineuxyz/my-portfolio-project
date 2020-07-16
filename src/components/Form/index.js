6becf16f6b331da90044d9f88bd115b78c827ab4
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-16 13:03:06 -0300

    finishing form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
 
-import { Container, Title, Description, Form, InputGroup, InputName, InputPhone } from './styles';
+import { 
+  Container, 
+  Title, 
+  Description, 
+  Form,
+  InputGroup,
+  InputName,
+  InputPhone,
+  InputEmail,
+  TextArea,
+  ContactInfos,
+} from './styles';
 
 export default function Formularie() {
+
+  async function handleNewIncident(event) {
+    event.preventDefault();
+  }
+
   return (
     <Container id="form">
-      <Title>Um titulo</Title>
-      <Description>Um subtitulo um subtitulo um subtitulo</Description>
+      <Title>Contact me</Title>
+      <Description>Here you can hire for some type of project. Tell me your information and describe the project that I will answer, I promise.</Description>
 
-      <Form>
+      <Form onSubmit={handleNewIncident} name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="hidden" name="form-name" value="contact"/>
         <InputGroup>
-          <InputName type="text"/>
-          <InputPhone type="text"/>
-        </InputGroup>
-        <input type="text"/>
-
-        <textarea />
-
-        <InputGroup>
-          <input type="checkbox" name="" id=""/>
-          <input type="checkbox" name="" id=""/>
-          <input  type="checkbox" name="" id=""/>
+          <InputName type="text" placeholder="Name"/>
+          <InputPhone type="text" placeholder="Your best phone"/>
         </InputGroup>
 
+        <InputEmail type="e-mail" placeholder="Your best email"/>
 
-        <input type="checkbox" name="" id="teste"/>
-        <button type="submit">Butão</button>
+        <TextArea placeholder="Tell me about your project, say everything!" />
+
+        <ContactInfos>
+          <p>If you prefer, you can send me an email to speak directly with me, ok? =)</p>
+          <span><address><a href="mailto:falacmg@lineupastorelli.com">falacmg@lineupastorelli.com</a></address></span>
+        </ContactInfos>
+
+        <button type="submit">Submit</button>
       </Form>
     </Container>
   );
