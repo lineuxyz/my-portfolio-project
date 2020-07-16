31e1acbe6645271ebb122bacd557073e39187900
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-16 13:02:05 -0300

    created new footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
 
 export const Copy = styled.div`
-      background-color: #353535;
-      display: flex;
-      justify-content: space-between;
-      padding: 15px 40px 10px 40px;
-      color: #fff;
-      font-size: 1.1rem;
+  height: 38px;
+
+  background-color: #353535;
+
+  margin-top: 20px;
+
+  bottom: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 50px;
+  color: #fff;
+  font-size: 1.1rem;
+
     a {
       color: #FFC400;
   }
