9f4699fceb0dd3db5344124dec220f095b25dc5c
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-21 14:55:31 -0300

    add new images to the demonstre projects

--- src/components/NewLearning/index.js
+++ src/components/NewLearning/index.js
@@ -10,6 +10,8 @@ import developing from '../../assets/developing.png';
 import BeTheHeroLogin from '../../assets/be_the_hero.png';
 import BeTheHeroRegister from '../../assets/be_the_hero_1.png';
 import DevSearch from '../../assets/dev_search.png';
+import DiscordClone from '../../assets/discord_clone.png';
+import GithubExplorer from '../../assets/github_explorer.png'
 
 
 export default function Three() {
@@ -20,12 +22,17 @@ export default function Three() {
           <div className="content">
             <p>My latest projects</p>
             <span>Clicking on the image you will be taken to the repository</span>
-            <div className="stream safe-area" > 
-              <a target="_blank" rel="noreferrer" href="https://bit.ly/3edYGDX"><img className="stream__thumbnail" src={BeTheHeroLogin} alt="" /></a>
+            
+            <div className="stream safe-area" >
+              <a target="_blank" rel="noreferrer" href="https://bit.ly/3jqlP9V"><img className="stream__thumbnail" src={DiscordClone} alt="Discord clone repo" /></a>
             </div>
 
             <div className="stream safe-area" > 
-              <a target="_blank" href="https://bit.ly/3edYGDX" rel="noreferrer"><img className="stream__thumbnail" src={BeTheHeroRegister} alt="" /></a>
+              <a target="_blank" rel="noreferrer" href="https://bit.ly/3hq67cS"><img className="stream__thumbnail" src={GithubExplorer} alt="Github explorer repo" /></a>
+            </div>
+
+            <div className="stream safe-area" > 
+              <a target="_blank" href="https://bit.ly/3edYGDX" rel="noreferrer"><img className="stream__thumbnail" src={BeTheHeroRegister} alt="Be the hero repo" /></a>
             </div>
 
             <div className="stream" > 
